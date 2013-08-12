da8a1ccdd9c96224adb86dd2755708ad15083124
dflyte <dfl@sandag.org>
2013-08-12 10:19:54 -0700

    Formatting code to be CheckStyle-compliant. (PECAS-76)

--- src/main/java/com/hbaspecto/pecas/ZipMatrixToCSV.java
+++ src/main/java/com/hbaspecto/pecas/ZipMatrixToCSV.java
@@ -1,20 +1,18 @@
 package com.hbaspecto.pecas;
 
 import java.io.File;
-
 import com.pb.common.matrix.CSVMatrixWriter;
 import com.pb.common.matrix.ZipMatrixReader;
 
-public class ZipMatrixToCSV {
+public class ZipMatrixToCSV
+{
+    public static void main(String[] args)
+    {
+        final File fromFile = new File(args[0]);
+        final File toFile = new File(args[1]);
+        final ZipMatrixReader reader = new ZipMatrixReader(fromFile);
+        final CSVMatrixWriter writer = new CSVMatrixWriter(toFile);
 
-	public static void main(String[] args) {
-		final File fromFile = new File(args[0]);
-		final File toFile = new File(args[1]);
-
-		final ZipMatrixReader reader = new ZipMatrixReader(fromFile);
-		final CSVMatrixWriter writer = new CSVMatrixWriter(toFile);
-
-		writer.writeMatrix(reader.readMatrix());
-	}
-
+        writer.writeMatrix(reader.readMatrix());
+    }
 }
